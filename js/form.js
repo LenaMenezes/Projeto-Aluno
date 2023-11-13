75e5f343d49a34487fb33031f8ad75ade87c1227
LenaMenezes <69567720+mateuscoripiom@users.noreply.github.com>
2023-11-13 08:38:44 -0300

    adicionando linhas form de notas

--- js/form.js
+++ js/form.js
@@ -1,17 +1,54 @@
 const botao = document.querySelector('#adicionar-aluno')
-
+ 
 // criando um ouvinte para o btn para tirar a acao de submit do btn
 botao.addEventListener('click',function(){  
     event.preventDefault() // tirando o submit
     let formulario = document.querySelector('#form-aluno') // pegando tds os objs do form
     let dadosAluno = novoAluno(formulario)
-
+/*
     console.log(dadosAluno.Jnome)
     console.log(dadosAluno.JN1.toFixed(1))
     console.log(dadosAluno.JN2.toFixed(1))
     console.log(dadosAluno.JN3.toFixed(1))
-    console.log(dadosAluno.JMediaFinal.toFixed(1))
+    console.log(dadosAluno.JMediaFinal.toFixed(2))
     console.log(dadosAluno.JSituacao)
+*/
+
+    //CRIANDO UMA NOVA LINHA NA TABELA
+
+    let linha = document.createElement('tr')
+
+    // criando as células vulgo td
+    let tdNome = document.createElement('td')
+    let tdN1 = document.createElement('td')
+    let tdN2 = document.createElement('td')
+    let tdN3 = document.createElement('td')
+    let tdMediaFinal = document.createElement('td')
+    let tdSituacao = document.createElement('td')
+
+    // inserindo elementos dentrod as td's
+    tdNome.textContent = dadosAluno.Jnome
+    tdN1.textContent = dadosAluno.JN1.toFixed(1)
+    tdN2.textContent = dadosAluno.JN2.toFixed(1)
+    tdN3.textContent = dadosAluno.JN3.toFixed(1)
+    tdMediaFinal.textContent = dadosAluno.JMediaFinal.toFixed(2)
+    tdSituacao.textContent = dadosAluno.JSituacao
+    tdSituacao.classList.add(MostrarSituacao(dadosAluno.JMediaFinal)[1])
+
+    linha.appendChild(tdNome)
+    linha.appendChild(tdN1)
+    linha.appendChild(tdN2)
+    linha.appendChild(tdN3)
+    linha.appendChild(tdMediaFinal)
+    linha.appendChild(tdSituacao)
+
+    let tabela = document.querySelector('#tabela-aluno')
+    tabela.appendChild(linha)
+
+    formulario.reset() // reseta o formulario
+    formulario.querySelector('#nome').focus() // foco no campo nome     
+
+
 })
 
             //VERIFICAÇÕES
@@ -88,9 +125,9 @@ function novoAluno (meuform){
     else{
         let jsonAluno = {
             Jnome: meuform.querySelector('#nome').value,
-            JN1: meuform.querySelector('#n1').value,
-            JN2: meuform.querySelector('#n2').value,
-            JN3: meuform.querySelector('#n3').value, 
+            JN1:parseFloat(meuform.querySelector('#n1').value.replace(",",".")),
+            JN2:parseFloat( meuform.querySelector('#n2').value.replace(",",".")),
+            JN3: parseFloat(meuform.querySelector('#n3').value.replace(",",".")), 
 
             JMediaFinal: calcularMedia(meuform.querySelector('#n1').value.replace(",","."),
             meuform.querySelector('#n2').value.replace(",","."),
